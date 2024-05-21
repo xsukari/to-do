59705ce00e910af6c21575d8a22df463322ffb69
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-21 15:31:48 +0200

    added theme to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "./theme"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,9 +19,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </head>
             <body className={inter.className}>
-                <AppRouterCacheProvider>
-                    {children}
+                <AppRouterCacheProvider /*options={{ enableCssLayer: true }}*/>
+                    <ThemeProvider theme={theme}>
+                        {children}
+                    </ThemeProvider>
                 </AppRouterCacheProvider>
             </body>
         </html>
