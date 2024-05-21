43655046d39c5a869a568348e60c23c5f7f8a8c8
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-21 15:31:33 +0200

    removed example accordion from page (was moved to notes component for now)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
-import Accordion from "@mui/material/Accordion"
-import AccordionSummary from "@mui/material/AccordionSummary"
-import AccordionDetails from "@mui/material/AccordionDetails"
-import Typography from "@mui/material/Typography"
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward"
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
-
+import Notes from "./components/notes"
 
 export default function Home() {
     return (
-        <main className="flex min-h-[1178px] min-w-[833px] max-h-[1180px] max-w-[833px] m-auto">
-            <div className="w-full bg-[#3b9aad]">
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ArrowDownwardIcon />}
-                        aria-controls="panel1-content"
-                        id="panel1-header"
-                    >
-                        <Typography>Accordion 1</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
+        <main className="flex min-h-screen min-w-screen m-auto">
+            <div className="w-full bg-[#009688]">
+                <div className="w-3/4 m-auto">
+                    <Notes />
+                </div>
             </div>
         </main>
     )
