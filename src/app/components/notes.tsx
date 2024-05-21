d68ee318c0df17aa501c21ba0a75971c4f1c8815
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-21 15:30:54 +0200

    added notes component

---
+++ src/app/components/notes.tsx
@@ -0,0 +1,29 @@
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import Typography from "@mui/material/Typography"
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "../theme"
+
+export default function notes() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </ThemeProvider>
+    )
+}
