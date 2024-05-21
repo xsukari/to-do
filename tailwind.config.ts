dac21ec89dae0553b7d21523ece5c30bd7712e3b
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-21 15:30:44 +0200

    removed unneeded lines from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,17 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      width: {
-         a4: '210mm',
-      },
-      height: {
-        a4: '297mm',
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
