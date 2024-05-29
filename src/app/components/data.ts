df9b70126ef2a66f1964176e144acff22a2164a8
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-29 14:53:47 +0200

    added example data

---
+++ src/app/components/data.ts
@@ -0,0 +1,68 @@
+export interface Task {
+    name: string,
+    due: string,
+    completed: boolean
+}
+
+export interface ToDo {
+    date: string,
+    tasks: Task[]
+}
+
+export const toDos = [
+    {
+        date: "2024-05-11",
+        tasks: [
+            {
+                name: "clean up counter",
+                due: "14:30:00",
+                completed: true
+            },
+            {
+                name: "wash clothes",
+                due: "15:15:00",
+                completed: true
+            },
+            {
+                name: "meet mia",
+                due: "17:10:00",
+                completed: false
+            },
+            {
+                name: "cook",
+                due: "18:45:00",
+                completed: false
+            },
+        ]
+    },
+    {
+        date: "2024-05-15",
+        tasks: [
+            {
+                name: "go grocery shopping",
+                due: "19:00:00",
+                completed: false
+            },
+            {
+                name: "download more ram",
+                due: "20:45:00",
+                completed: false
+            },
+        ]
+    },
+    {
+        date: "2024-05-19",
+        tasks: [
+            {
+                name: "ask how much dedotated wam for minecraft server",
+                due: "14:00:00",
+                completed: false
+            },
+            {
+                name: "new wallpaper",
+                due: "20:45:00",
+                completed: false
+            },
+        ]
+    },
+]
