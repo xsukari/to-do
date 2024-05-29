212ef652e9b7fef8c873947ff1485b96b67807f1
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-29 12:07:25 +0200

    replaced color value with new tailwind variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Notes from "./components/notes"
 export default function Home() {
     return (
         <main className="flex min-h-screen min-w-screen m-auto">
-            <div className="w-full bg-[#009688]">
+            <div className="w-full bg-main">
                 <div className="w-3/4 m-auto">
                     <Notes />
                 </div>
