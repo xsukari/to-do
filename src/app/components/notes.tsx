702f5e5813a2d6a9cb803702d02a28b7c37fe780
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-29 12:09:38 +0200

    added and adjusted the "only one expanded at a time" accordion example from the MUI docs

--- src/app/components/notes.tsx
+++ src/app/components/notes.tsx
@@ -1,29 +1,109 @@
-import Accordion from "@mui/material/Accordion"
-import AccordionSummary from "@mui/material/AccordionSummary"
-import AccordionDetails from "@mui/material/AccordionDetails"
+"use client"
 import Typography from "@mui/material/Typography"
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
-import { ThemeProvider } from "@mui/material/styles"
-import theme from "../theme"
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp"
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion"
+import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary"
+import MuiAccordionDetails from "@mui/material/AccordionDetails"
+import { styled } from "@mui/material/styles"
+import React from "react"
 
-export default function notes() {
+const Accordion = styled((props: AccordionProps) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+    border: `0px solid ${theme.palette.divider}`,
+    "&:not(:last-child)": {
+        borderBottom: 0,
+    },
+    "&::before": {
+        display: "none",
+    },
+}))
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: "0px solid rgba(0, 0, 0, .125)",
+}))
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem", color: "secondary.main" }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    backgroundColor:
+        theme.palette.mode === "dark"
+            ? "rgba(255, 255, 255, .05)"
+            : "rgba(0, 0, 0, .03)",
+    flexDirection: "row-reverse",
+    "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+        transform: "rotate(90deg)",
+    },
+    "& .MuiAccordionSummary-content": {
+        marginLeft: theme.spacing(1),
+    },
+}))
+
+export default function Notes() {
+    const [expanded, setExpanded] = React.useState<string | false>("panel1")
+
+    const handleChange =
+        (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+            setExpanded(newExpanded ? panel : false)
+        }
+
     return (
-        <ThemeProvider theme={theme}>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ArrowDropDownIcon />}
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                >
-                    <Typography>Accordion 1</Typography>
+        <div>
+            <Accordion 
+                expanded={expanded === "panel1"} onChange={handleChange("panel1")}
+                slotProps={{ transition: { unmountOnExit: true } }}
+                sx={{ bgcolor: "primary.main", color: "text.primary" }}
+            >
+                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                    <Typography>Collapsible Group Item #1</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
-        </ThemeProvider>
+            <Accordion 
+                expanded={expanded === "panel2"} onChange={handleChange("panel2")}
+                slotProps={{ transition: { unmountOnExit: true } }}
+                sx={{ bgcolor: "primary.main", color: "text.primary" }}
+            >
+                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                    <Typography>Collapsible Group Item #2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion 
+                expanded={expanded === "panel3"} onChange={handleChange("panel3")}
+                slotProps={{ transition: { unmountOnExit: true } }}
+                sx={{ bgcolor: "primary.main", color: "text.primary" }}
+            >
+                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+                    <Typography>Collapsible Group Item #3</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
     )
 }
