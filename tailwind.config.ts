15ab6e747d7f23e05c650761a0fa7c6142651c72
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-29 12:07:09 +0200

    added the main (background) color to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "main" : "#009688",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
