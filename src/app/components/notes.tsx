74bc703fab83a81d6f8ad237fb8edd336571aa59
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-29 15:28:16 +0200

    deleted unused code

--- src/app/components/notes.tsx
+++ src/app/components/notes.tsx
@@ -107,43 +107,4 @@ export default function Notes() {
     })
 
     return elements
-    /*
-        <div>
-
-            <Accordion 
-                expanded={expanded === "panel2"} onChange={handleChange("panel2")}
-                slotProps={{ transition: { unmountOnExit: true } }}
-                sx={{ bgcolor: "primary.main", color: "text.primary" }}
-            >
-                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-                    <Typography>Collapsible Group Item #2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion 
-                expanded={expanded === "panel3"} onChange={handleChange("panel3")}
-                slotProps={{ transition: { unmountOnExit: true } }}
-                sx={{ bgcolor: "primary.main", color: "text.primary" }}
-            >
-                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-                    <Typography>Collapsible Group Item #3</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-        </div>
-    */
 }
