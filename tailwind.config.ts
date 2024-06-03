e0c417bddccd06cd1eb94335551208b6762a90bf
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-03 12:10:22 +0200

    adjusted indentation to match the rest of the typescript files and added a color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "main" : "#009688",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "main" : "#009688",
+                "control": "rgba(0, 0, 0, .20)"
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+            },
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [],
+}
+export default config
