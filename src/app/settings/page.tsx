60fa6dde20a4c5f1349f972535c36f60304927de
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-05 15:05:27 +0200

    added navbar to /settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/navbar"
 export default function Home() {
     return (
         <main className="flex min-h-screen min-w-screen m-auto">
             <div className="w-full bg-main">
                 <div className="bg-control">
-
+                    <Navbar />
                 </div>
 
                 <div className="w-3/4 m-auto mt-12">
