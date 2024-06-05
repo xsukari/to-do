9568bdf3306d596cdda6123d9210d9f51b1939d0
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-05 15:10:16 +0200

    added user and admin settings components to /settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,4 +1,7 @@
 import Navbar from "./components/navbar"
+import UserSettings from "./components/user_settings"
+import AdminSettings from "./components/admin_settings"
+
 export default function Home() {
     return (
         <main className="flex min-h-screen min-w-screen m-auto">
@@ -8,7 +11,8 @@ export default function Home() {
                 </div>
 
                 <div className="w-3/4 m-auto mt-12">
-
+                    <UserSettings />
+                    <AdminSettings />
                 </div>
             </div>
         </main>
