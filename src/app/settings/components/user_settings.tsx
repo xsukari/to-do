4fd0591d614f27ecda72d68bddc901c6087b4246
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-10 12:50:55 +0200

    finished user settings for now

--- src/app/settings/components/user_settings.tsx
+++ src/app/settings/components/user_settings.tsx
@@ -4,17 +4,24 @@ import { Typography, InputAdornment } from "@mui/material"
 import TextField from "@mui/material/TextField"
 
 export default function UserSettings() {
-    const [remindAfter, setRemindAfter] = React.useState<string | null>("0")
+    const [remindAfter, setRemindAfter] = React.useState<string | null>("")
 
     return (
-        <div className="w-full flex bg-panel">
-            <div className="w-1/3">
-                <Typography>TEST</Typography>
+        <div className="w-full flex bg-panel items-center py-2">
+            <div className="w-1/3 px-2">
+                <Typography>
+                    Additional reminder after
+                </Typography>
+                <Typography variant="caption">
+                    0 or no value disables it
+                </Typography>
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 px-2">
                 <TextField
                     required
                     value={remindAfter}
+                    size="small"
+                    placeholder="15"
                     onChange={
                         (event: React.ChangeEvent<HTMLInputElement>) => {
                             setRemindAfter(event.target.value)
