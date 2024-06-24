5bb76f203dc1913c94cac380420dc720aa1010c9
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-24 12:43:54 +0200

    added more screen size breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,15 @@ const config: Config = {
             },
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            screens: {
+                "3xl": "2048px",
+                "4xl": "2560px",
+                "5xl": "3072px",
+                "6xl": "4096px",
+                "7xl": "5120px",
+                "8xl": "6144px",
             },
         },
     },
