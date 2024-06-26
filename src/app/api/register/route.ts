0764c7df65f5b31aa15565b908d4934994ff73b4
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-26 16:04:00 +0200

    added mostly complete registration API function

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,132 @@
+import { Auth } from "../../data/types"
+import { 
+    usersExist,
+    invitedAndValid,
+    newUser,
+    usernameTaken, 
+    updateInvitation,
+    removeUser,
+} from "../../data/database/functions"
+import vine, { errors } from "@vinejs/vine"
+import bcrypt from "bcrypt"
+
+async function validate(auth: Auth) {
+    const schema = vine.object({
+        username: vine.string(),
+        email: vine.string().email(),
+        password: vine
+            .string()
+            .minLength(8)
+            .maxLength(32)
+            .confirmed()
+    })
+
+    const data = {
+        username: auth.username,
+        email: auth.email,
+        password: auth.password,
+        password_confirmation: auth.passwordConfirmation,
+    }
+
+    try {
+        const output = await vine.validate({ schema, data })
+
+        const validatedData = {
+            email: output.email,
+            username: output.username,
+            password: output.password,
+        }
+
+        return validatedData
+    } catch (error) {
+        if (error instanceof errors.E_VALIDATION_ERROR) {
+            console.log(error.messages)
+        }
+
+        return null
+    }
+}
+
+export async function POST(request: Request) {
+    const data = {
+        message: ""
+    }
+
+    const auth = await request.json() as Auth
+    const validatedAuth = await validate(auth)
+
+    if (!validatedAuth) {
+        data.message = "Invalid credentials, please check your email / password."
+
+        return Response.json(data)
+    }
+
+    /**
+     * amount of bcrypt salt rounds:
+     * aim for a computation time of ~ 250 ms
+     * 
+     * 13 rounds are ~ 300 ms
+     * 12 rounds are ~ 150 ms
+     * using 7800X3D
+     */
+    // const start = new Date().getTime()
+    const hashedPassword = await bcrypt.hash(validatedAuth.password, 13) 
+    // const end = new Date().getTime()
+    // console.log("\n" + (end - start) + " milliseconds\n")
+
+    if (!await usersExist()) {
+        try {
+            await newUser(
+                validatedAuth.username,
+                validatedAuth.email,
+                hashedPassword,
+                true
+            )
+
+            data.message = "Registration successful."
+        } catch (error) {
+            data.message = "Registration unsuccessful."
+        }
+
+        return Response.json(data)
+    } else {
+        const isInvitedAndValid = await invitedAndValid(validatedAuth.email)
+        const isUsernameNotTaken = !await usernameTaken(validatedAuth.username)
+
+        if (isInvitedAndValid && isUsernameNotTaken) {
+            let userCreationFailed = false
+
+            try {
+                await newUser(
+                    validatedAuth.username,
+                    validatedAuth.email,
+                    hashedPassword,
+                    false
+                )
+            } catch (error) {
+                userCreationFailed = true
+            }
+
+            if (!userCreationFailed) {
+                try {
+                    await updateInvitation(validatedAuth.email)
+
+                    data.message = "Registration successful."
+                } catch (error) {
+                    try {
+                        await removeUser(validatedAuth.username, validatedAuth.email)
+                        
+                        data.message = "Registration unsuccessful."
+                    } catch (error) {
+                        // Add sending of mails to admins here
+                        data.message = "Error. Contact your administrator."
+                    }
+                }
+            }
+        } else {
+            data.message = "You are not invited or your username is already taken."
+        }
+
+        return Response.json(data)
+    }
+}
