2cdc08c4be0c8517e4e883927fbc47643746089a
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-26 16:02:08 +0200

    updated usersExist API function

--- src/app/api/users-exist/route.ts
+++ src/app/api/users-exist/route.ts
@@ -3,7 +3,7 @@ import { db } from "../../data/database/database"
 export async function GET() {
     const query = db.selectFrom("User").select("id").limit(1)
 
-    const dbData = await query.execute()
+    const dbData = await query.executeTakeFirst()
 
     const data = {
         message: dbData ? "true" : "false"
