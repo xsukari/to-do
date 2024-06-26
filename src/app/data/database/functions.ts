14d45bb481e4383aa2e501460fae7a08409869f0
xsukari <43860627+xsukari@users.noreply.github.com>
2024-06-26 16:03:15 +0200

    added a few database functions

---
+++ src/app/data/database/functions.ts
@@ -0,0 +1,76 @@
+import { db } from "../../data/database/database"
+import { NewUser, InviteUpdate } from "../../data/database/database_types"
+
+export async function usersExist() {
+    const query = 
+        db.selectFrom("User")
+            .select("id")
+            .limit(1)
+
+    const dbData = await query.executeTakeFirst()
+
+    return dbData ? true : false
+}
+
+export async function invitedAndValid(email: string) {
+    const query = 
+        db.selectFrom("Invite")
+            .select("id")
+            .where("email", "=", email)
+            .where("valid", "=", true)
+            .where("registered", "=", false)
+            .limit(1)
+
+    const dbData = await query.executeTakeFirst()
+
+    return dbData ? true : false
+}
+
+export async function newUser(username: string, email: string, hashedPassword: string, admin: boolean) {
+    const user: NewUser = {
+        username: username,
+        email: email,
+        password: hashedPassword,
+        admin: admin,
+        updatedAt: new Date().toISOString(),
+    }
+
+    const query = db.insertInto("User").values(user)
+
+    await query.executeTakeFirst()
+}
+
+export async function usernameTaken(username: string) {
+    const query = 
+        db.selectFrom("User")
+            .select("id")
+            .where("username", "=", username)
+
+    const dbData = await query.executeTakeFirst()
+
+    return dbData ? true : false
+}
+
+export async function updateInvitation(email: string) {
+    const invite: InviteUpdate = {
+        valid: false,
+        registered: true,
+        updatedAt: new Date(),
+    }
+
+    const query = 
+        db.updateTable("Invite")
+            .set(invite)
+            .where("email", "=", email)
+
+    await query.executeTakeFirst()
+}
+
+export async function removeUser(username: string, email: string) {
+    const query = 
+        db.deleteFrom("User")
+            .where("username", "=", username)
+            .where("email", "=", email)
+
+    await query.executeTakeFirst()
+}
