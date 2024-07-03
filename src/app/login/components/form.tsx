cc90dc40a59ec8d2a2aa45607950e94bf59cf97c
xsukari <43860627+xsukari@users.noreply.github.com>
2024-07-03 13:42:36 +0200

    adjusted login form based on the improvements to the registration form

--- src/app/login/components/form.tsx
+++ src/app/login/components/form.tsx
@@ -6,6 +6,8 @@ import { Typography } from "@mui/material"
 import Link from "next/link"
 import useSWRMutation from "swr/mutation"
 import { Credentials } from "../../data/types"
+import Alert from "@mui/material/Alert"
+import { useRouter } from "next/navigation"
 
 const fetcher = async (url: string, { arg }: { arg: Credentials }) => {
     const response = await fetch(url, {
@@ -27,60 +29,84 @@ export default function Form() {
     const [username, setUsername] = React.useState<string>("")
     const [password, setPassword] = React.useState<string>("")
 
-    const { trigger, isMutating/*, data, error*/ } = useSWRMutation("/api/login", fetcher)
+    const { trigger, isMutating } = useSWRMutation("/api/login", fetcher)
+
+    const router = useRouter()
+    const [message, setMessage] = React.useState<string>("")
+    const [success, setSuccess] = React.useState<boolean>(false)
 
     const handleSubmit = async () => {
-        const response = await trigger({ username: username, password: password } as Credentials)
-        
-        console.log(response.message)
+        const response = await trigger({ 
+            username: username,
+            password: password,
+        } as Credentials)
+
+        setSuccess(response.message.success)
+        setMessage(response.message.text)
+
+        if (response.message.success) {
+            setTimeout(() => {
+                router.push("/")
+            }, 5000)
+        }
+
+        setTimeout(() => {
+            setMessage("")
+        }, 10000)
     }
 
     return (
-        <div className="w-full bg-panel lg:w-1/2 2xl:w-1/3 m-auto">
-            <div className="py-2">
-                <div className="flex-grow px-2 pb-2">
-                    <TextField
-                        label="Username"
-                        value={username}
-                        size="small"
-                        onChange={
-                            (event: React.ChangeEvent<HTMLInputElement>) => {
-                                setUsername(event.target.value)
+        <div className="flex flex-col min-h-screen">
+            <div className="h-12">
+                {message && <Alert severity={success ? "success" : "error"}>{message}</Alert>}
+            </div>
+            <div className="w-full bg-panel lg:w-1/2 2xl:w-1/3 m-auto">
+                <div className="py-2">
+                    <div className="flex-grow px-2 pb-2">
+                        <TextField
+                            label="Username"
+                            value={username}
+                            size="small"
+                            onChange={
+                                (event: React.ChangeEvent<HTMLInputElement>) => {
+                                    setUsername(event.target.value)
+                                }
                             }
-                        }
-                        className="w-full"
-                    />
-                </div>
-                <div className="flex-grow px-2 pb-2">
-                    <TextField
-                        label="Password"
-                        value={password}
-                        size="small"
-                        onChange={
-                            (event: React.ChangeEvent<HTMLInputElement>) => {
-                                setPassword(event.target.value)
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="flex-grow px-2 pb-2">
+                        <TextField
+                            label="Password"
+                            value={password}
+                            size="small"
+                            onChange={
+                                (event: React.ChangeEvent<HTMLInputElement>) => {
+                                    setPassword(event.target.value)
+                                }
                             }
-                        }
-                        className="w-full"
-                    />
-                </div>
-                <div className="px-2 pb-2">
-                    <Link href="/register">
-                        <Typography variant="caption">
-                                Not registered yet?
-                        </Typography>
-                    </Link>
-                </div>
-                <div className="w-1/4 px-2 m-auto">
-                    <Button
-                        variant="outlined" 
-                        className="w-full"
-                        onClick={() => {
-                            trigger({ username: username, password: password } as Credentials)
-                        }}
-                    >
-                        Login
-                    </Button>
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="px-2 pb-2">
+                        <Link href="/register">
+                            <Typography variant="caption">
+                                    Not registered yet?
+                            </Typography>
+                        </Link>
+                    </div>
+                    <div className="w-1/4 px-2 m-auto">
+                        <Button
+                            variant="outlined" 
+                            className="w-full"
+                            disabled={isMutating}
+                            onClick={() => {
+                                handleSubmit()
+                            }}
+                        >
+                            Login
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
